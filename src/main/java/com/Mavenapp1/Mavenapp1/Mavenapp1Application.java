e715ac3a0af4cceae95f34fce75fd6886065f4b9
ravishankaratavalgi <Ravishankaratavalgi.1999@gmail.com>
2022-10-30 10:10:54 +0530

    rtgyhyy7gyhy

---
+++ src/main/java/com/Mavenapp1/Mavenapp1/Mavenapp1Application.java
@@ -0,0 +1,13 @@
+package com.Mavenapp1.Mavenapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mavenapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mavenapp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Mavenapp1/Mavenapp1/Mavenapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Mavenapp1.Mavenapp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mavenapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
